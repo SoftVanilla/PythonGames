4570eb92c1b2d707f86c196f18813805243102c3
SoftVanilla <52844822+SoftVanilla@users.noreply.github.com>
2020-11-04 19:48:40 +0900

    Update code.py

--- tic-tac-toe/code.py
+++ tic-tac-toe/code.py
@@ -1,60 +1,53 @@
-board = [['.' for i in range(3)] for j in range(3)]
+l = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
 
-def printBoard():
-    for line in board:
+cnt = 0
+mark = 'X'
+
+for line in l:
+    for ch in line:
+        print(ch, end=' ')
+    print()
+
+while cnt != 9:
+    while True:
+        try:
+            x, y = map(int, input('어디에 표시할까요? ').split())
+        except:
+            print("잘못된 입력!")
+            continue
+
+        if 0 <= x <= 2 and 0 <= y <= 2:
+            if l[x][y] == '.':
+                l[x][y] = mark
+                break
+            else:
+                print('이미 표시된 곳입니다.')
+                continue
+        print("잘못된 입력!")
+
+    for line in l:
         for ch in line:
             print(ch, end=' ')
         print()
 
-def checkWin(ch):
-    for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] == ch:
-            return True
-        if board[0][i] == board[1][i] == board[2][i] == ch:
-            return True
-    if board[0][0] == board[1][1] == board[2][2] == ch:
-        return True
-    if board[2][0] == board[1][1] == board[0][2] == ch:
-        return True
-    return False
+    fin = False
 
-print('Let\'s Play Tic-Tac-Toe!')
-print('Enter the number like this : ')
+    if mark == l[0][0] == l[0][1] == l[0][2] or\
+    mark == l[1][0] == l[1][1] == l[1][2] or\
+    mark == l[2][0] == l[2][1] == l[2][2] or\
+    mark == l[0][0] == l[1][0] == l[2][0] or\
+    mark == l[0][1] == l[1][1] == l[2][1] or\
+    mark == l[0][2] == l[1][2] == l[2][2] or\
+    mark == l[0][0] == l[1][1] == l[2][2] or\
+    mark == l[0][2] == l[1][1] == l[2][0]:
+        fin = True
 
-for i in range(3):
-    for j in range(3):
-        print(3*i+j+1, end=' ')
-    print()
-
-turn = 1
-cnt = 0
-txt = 'O'
-
-while cnt != 9:
-    print('Player', turn, 'turn.')
-    while True:
-        v = int(input('Enter the number to place : '))
-        if 1 <= v <= 9:
-            v -= 1
-            if board[v//3][v%3] == '.':
-                board[v//3][v%3] = txt
-                break
-            else:
-                print('Already placed...')
-        else:
-            print('Invalid place...')
-
-    printBoard()
-    if checkWin(txt):
-        print('Player', turn, 'Win!!!')
+    if fin:
+        print(mark, "승리!")
         break
-    cnt += 1
-    if turn == 1:
-        turn = 2
-        txt = 'X'
-    else:
-        turn = 1
-        txt = 'O'
 
-if cnt == 9:
-    print('Draw game...')
+    cnt += 1
+    if mark == 'X':
+        mark = 'O'
+    else:
+        mark = 'X'
